134fbad91eee2e85d6c8e73885f48d3721051fe5
BOBBY <bobbypt05@gmail.com>
2018-03-10 23:02:14 +0530

    First commit

---
+++ app/src/main/java/com/example/android/campusrecruitmentsystem/doSignUp.java
@@ -0,0 +1,116 @@
+package com.example.android.campusrecruitmentsystem;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.android.campusrecruitmentsystem.R;
+import com.example.android.campusrecruitmentsystem.login;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class doSignUp extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText mUserFullName;
+    private EditText mUserName;
+    private EditText mUserId;
+    private EditText mUserEmailId;
+    private EditText mUserContactNo;
+    private EditText mUserPassword;
+    private EditText mUserConfirmPassword;
+    private Button mSignUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_do_sign_up);
+        
+        /**code for spinner**/
+        Spinner spinner = (Spinner) findViewById(R.id.signup_spinner_id);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.signup_spinner_id, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        /**code for spinner**/
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mUserFullName = (EditText) findViewById(R.id.UserFullName);
+        mUserName = (EditText) findViewById(R.id.Username);
+        mUserId = (EditText) findViewById(R.id.userId);
+        mUserEmailId = (EditText) findViewById(R.id.userEmailId);
+        mUserContactNo = (EditText) findViewById(R.id.userContactNo);
+        mUserPassword = (EditText) findViewById(R.id.userPassword);
+        mUserConfirmPassword = (EditText) findViewById(R.id.userConfirmPassword);
+        mSignUpButton = (Button) findViewById(R.id.signUpButton);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = mUserEmailId.getText().toString();
+                String password = mUserPassword.getText().toString();
+                String confirmPassword = mUserConfirmPassword.getText().toString();
+
+                if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(confirmPassword))
+                {
+                    if(password.equals(confirmPassword))
+                    {
+                        // bhai phod diya aur tu ab tera register ho sakta hai
+
+                        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    Intent i = new Intent(doSignUp.this,login.class);
+                                    startActivity(i);
+                                    finish();
+                                }
+                                else
+                                {
+                                    String errorMessage = task.getException().getMessage();
+                                    Toast.makeText(doSignUp.this,"Error: "+errorMessage,Toast.LENGTH_LONG);
+                                }
+                            }
+                        });
+                    }
+                    else
+                    {
+                        Toast.makeText(doSignUp.this, "Confirm Password & Password doesn't match",Toast.LENGTH_LONG).show();
+                    }
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null)
+        {
+            // kuch to karenge
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/campusrecruitmentsystem/login.java
@@ -0,0 +1,75 @@
+package com.example.android.campusrecruitmentsystem;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.android.campusrecruitmentsystem.R;
+import com.example.android.campusrecruitmentsystem.updateDetails;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText loginEmailText;
+    private EditText loginPasswordText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        mAuth = FirebaseAuth.getInstance();
+        loginEmailText = (EditText) findViewById(R.id.userEmail);
+        loginPasswordText = (EditText) findViewById(R.id.userPassword);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String loginEmail = loginEmailText.getText().toString();
+                String loginPassword = loginPasswordText.getText().toString();
+                if(!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty((loginPassword)))
+                {
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                Intent i = new Intent(login.this, updateDetails.class);
+                                startActivity(i);
+                                finish();
+                            }
+                            else
+                            {
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(login.this, "Error: " + errorMessage, Toast.LENGTH_LONG ).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser =  mAuth.getCurrentUser();
+
+    }
+}
